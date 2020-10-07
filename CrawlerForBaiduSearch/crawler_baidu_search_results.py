94b3433e5428291dc0add0ec1f0b3524a920f1cb
2437143099@qq.com <2437143099@qq.com>
2020-10-07 11:26:54 +0800

    update 3.0

--- CrawlerForBaiduSearch/crawler_baidu_search_results.py
+++ CrawlerForBaiduSearch/crawler_baidu_search_results.py
@@ -19,7 +19,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from urllib import request
 from urllib.request import quote
-
+global start
 
 driver_path = "C:\\Users\\Administrator\\Desktop\\Try Harder\\SRC漏洞\\自动化爬取漏洞\\chrome-win\\chromedriver.exe"
 
@@ -47,95 +47,136 @@ def random_userAgent(headersParameters):
 
 
 # 开始执行程序
-def run(path, keyword, num=5):
-    if num == 1:
-        print("☀✘✘✘-----Num needs to be greater than 1")
-        sys.exit()
-    else:
-        tips = "Starting the browser driver in about 3 seconds"
-        # print(tips)
-        bar = Bar("Starting the browser driver in about 3 seconds", max=10, fill="█", suffix="%(percent)d%%")
-        for i in range(10):
-            time.sleep(0.3)
-            bar.next()
-        bar.finish()
-        print("\n✈✈✈✈✈✈✈✈✈✈ Search info ✈✈✈✈✈✈✈✈✈✈\n The total number of pages is: {p}  search keyword is: {k}\n".format(p=num, k=keyword))
-        results_list = []  # 存储百度链接
-        cookie_dict = {}  # 存储cookie字典
-        click_num = num  # 设置点击页数，也是爬取的页数
-        get_current_page_xpath = ""  # 爬取当前页面的full_xpth
-        chrome_driver = driver_path
+def run(path, keyword, num):
+    global start
+    div_del = 0
+    tips = "Starting the browser driver in about 3 seconds"
+    # print(tips)
+    bar = Bar("Starting the browser driver in about 3 seconds", max=1, fill="█", suffix="%(percent)d%%")
+    for i in range(1):
+        time.sleep(0.1)
+        bar.next()
+    bar.finish()
+    print("\n✈✈✈✈✈✈✈✈✈✈ Search info ✈✈✈✈✈✈✈✈✈✈\n The total number of pages is: {p}  search keyword is: {k}\n".format(p=num, k=keyword))
+    results_list = []  # 存储百度链接
+    cookie_dict = {}  # 存储cookie字典
+    click_num = num  # 设置点击页数，也是爬取的页数
+    #print(type(click_num), click_num)
+    get_current_page_xpath = ""  # 爬取当前页面的full_xpth
+    chrome_driver = driver_path
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')  # 设置无界面模式运行浏览器
+    # chrome_options.add_argument('--incognito')  # 设置无痕模式
+    # chrome_options.add_argument('--disable-infobars')  # 设置禁用浏览器正在被自动化程序控制的提示
+    #chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+    driver.get("https://www.baidu.com/s?ie=UTF-8&wd={k}".format(k=urllib.parse.quote(keyword)))
+    time_sleep = random.randint(0, 3)
+    # 随机延时，避免百度安全机制识别
+    time.sleep(time_sleep)
+    #title = driver.title
+    # print(title)
+    # cookie获取、格式转换、设置cookie
+    cookie_list = driver.get_cookies()
+    for c in cookie_list:
+        cookie_dict[c['name']] = c['value']
+    cookies_str = json.dumps(cookie_dict)
+    cookies_str = cookies_str.replace('{', '').replace('}', '').replace(":", "=").replace('"', "").replace(" ", "").replace(",", ";").replace("=FG", ":FG")
+    headersParameters['Cookie'] = cookies_str
 
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')  # 设置无界面模式运行浏览器
-        # chrome_options.add_argument('--incognito')  # 设置无痕模式
-        # chrome_options.add_argument('--disable-infobars')  # 设置禁用浏览器正在被自动化程序控制的提示
-        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        driver = webdriver.Chrome(chrome_driver, options=chrome_options)
-        driver.get("https://www.baidu.com/s?ie=UTF-8&wd={k}".format(k=urllib.parse.quote(keyword)))
-        time_sleep = random.randint(0, 3)
-        # 随机延时，避免百度安全机制识别
-        time.sleep(time_sleep)
-        #title = driver.title
-        # print(title)
-        # cookie获取、格式转换、设置cookie
-        cookie_list = driver.get_cookies()
-        for c in cookie_list:
-            cookie_dict[c['name']] = c['value']
-        cookies_str = json.dumps(cookie_dict)
-        cookies_str = cookies_str.replace('{', '').replace('}', '').replace(":", "=").replace('"', "").replace(" ", "").replace(",", ";").replace("=FG", ":FG")
-        headersParameters['Cookie'] = cookies_str
-
-        # 循环点击页数1
-        print("Start crawl page {cn} ------------------------------------------------------".format(cn=(1)))
-        for num in range(1, 11):
-            try:
+    # 循环点击页数1
+    print("Start crawl page {cn} ------------------------------------------------------".format(cn=(1)))
+    for num in range(1, 11):
+        try:
+            if num == div_del:
+                pass
+            else:
                 get_link_xpath = "/html/body/div[1]/div[3]/div[1]/div[3]/div[" + str(num) + "]/h3/a"
                 # /html/body/div[1]/div[3]/div[1]/div[3]/div[4]/div
                 b = driver.find_element_by_xpath(get_link_xpath).get_attribute('href')
                 print(b)
                 results_list.append(b)
-            except Exception as e:
+        except Exception as e:
+            #raise e
+            try:
                 # 如果有推荐百度搜索div块进行+1
                 get_link_xpath = "/html/body/div[1]/div[3]/div[1]/div[3]/div[" + str(num + 1) + "]/h3/a"
+                # 挑出需要加1的DIV块并赋值保存
+                div_del = num + 1 
                 b = driver.find_element_by_xpath(get_link_xpath).get_attribute('href')
                 print(b)
                 results_list.append(b)
                 print("☀✔✔✔✔✔✔✔✔✔ Read the next div block ", get_link_xpath)
-                #raise e
-            finally:
-                pass
+            except Exception as e:
+                #print("-------------------------------There is no relevant link !!!")
+                break
+        finally:
+            pass
+    if click_num == 1:
+        geturl(results_list)
+        end = datetime.datetime.now()
+        print("\n☯☯☯☯☯☯☯☯ Program execution time is", end - start, "Everything is done, result.txt is saved current directory!")
+        sys.exit()
+    try:
         driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div[1]/a[1]/span[2]").click()
         time.sleep(2)
         # print(title)
-        print("Start crawl page {cn} ------------------------------------------------------".format(cn=(2)))
-        for num in range(1, 11):
-            try:
+    except Exception as e:
+        #raise e
+        geturl(results_list)
+        end = datetime.datetime.now()
+        print("\n☯☯☯☯☯☯☯☯ Program execution time is", end - start, "Everything is done, result.txt is saved current directory!")
+        sys.exit()
+    print("Start crawl page {cn} ------------------------------------------------------".format(cn=(2)))
+    for num in range(1, 11):
+        try:
+            if div_del == num:
+                pass
+            else:
                 get_link_xpath = "/html/body/div[1]/div[3]/div[1]/div[3]/div[" + str(num) + "]/h3/a"
                 # print(get_link_xpath)
                 b = driver.find_element_by_xpath(get_link_xpath).get_attribute('href')
                 print(b)
                 results_list.append(b)
-            except Exception as e:
+        except Exception as e:
+            try:
                 get_link_xpath = "/html/body/div[1]/div[3]/div[1]/div[3]/div[" + str(num + 1) + "]/h3/a"
+                div_del = num + 1
                 b = driver.find_element_by_xpath(get_link_xpath).get_attribute('href')
                 print(b)
                 results_list.append(b)
                 print("☀✔✔✔✔✔✔✔✔✔ Read the next div block ", get_link_xpath)
                 #raise e
-            finally:
-                pass
-        for click_n in range(3, click_num + 1):
-            if click_n > 7:
-                get_current_page_xpath = "/html/body/div[1]/div[3]/div[2]/div/a[7]/span[2]"
-            else:
-                get_current_page_xpath = "/html/body/div[1]/div[3]/div[2]/div/a[" + str(click_n) + "]/span[2]"
-            print("Start crawl page {cn} ------------------------------------------------------".format(cn=(click_n)))
-            # print(get_current_page_xpath)
+            except Exception as e:
+                print("-------------------------------There is no relevant link !!!")
+                break
+        finally:
+            pass
+    if click_num == 2:
+        geturl(results_list)
+        end = datetime.datetime.now()
+        print("\n☯☯☯☯☯☯☯☯ Program execution time is", end - start, "Everything is done, result.txt is saved current directory!")
+        sys.exit()
+    for click_n in range(3, click_num + 1):
+        if click_n > 7:
+            get_current_page_xpath = "/html/body/div[1]/div[3]/div[2]/div/a[7]/span[2]"
+        else:
+            get_current_page_xpath = "/html/body/div[1]/div[3]/div[2]/div/a[" + str(click_n) + "]/span[2]"
+        try:
             driver.find_element_by_xpath(get_current_page_xpath).click()
-            # print(title)
+            print("Start crawl page {cn} ------------------------------------------------------".format(cn=(click_n)))
+            print(get_current_page_xpath)
             time.sleep(2)
-            for num in range(1, 11):
+        except Exception as e:
+            #raise e
+            geturl(results_list)
+            end = datetime.datetime.now()
+            print("\n☯☯☯☯☯☯☯☯ Program execution time is", end - start, "Everything is done, result.txt is saved current directory!")
+            sys.exit()
+        for num in range(1, 11):
+            if num == div_del:
+                pass
+            else:
                 get_link_xpath = "/html/body/div[1]/div[3]/div[1]/div[3]/div[" + str(num) + "]/h3/a"
                 try:
                     b = driver.find_element_by_xpath(get_link_xpath).get_attribute('href')
@@ -143,69 +184,87 @@ def run(path, keyword, num=5):
                     results_list.append(b)
                     # time.sleep(2)  # 等待driver加载完成
                 except Exception as e:
-                    # 如果有推荐百度搜索div块进行+1
-                    get_link_xpath = "/html/body/div[1]/div[3]/div[1]/div[3]/div[" + str(num + 1) + "]/h3/a"
-                    b = driver.find_element_by_xpath(get_link_xpath).get_attribute('href')
-                    print("☀✔✔✔✔✔✔✔✔✔ Read the next div block ", get_link_xpath)  # 此时出现相关搜索的div块，对此div块已加1
-                    results_list.append(b)
-                    #print("error------------------", get_link_xpath)
-                    #raise e
-                finally:
-                    pass
-        # 对获取的百度链接进行处理
-        with open('result.txt', 'w', encoding='utf-8') as f:
-            with open('log.txt', 'w', encoding='utf-8') as f1:
-                print("\n☀☀☀ Get the real url -----------------------------------------------------------------")
-                for r in results_list:
                     try:
-                        last_url = ""
-                        headersParameters1 = random_userAgent(headersParameters)
-                        # 对获取的含有baidu连接的URL进行跳转
-                        baidu_url = requests.get(url=r, headers=headersParameters1, allow_redirects=False, timeout=3)
-                        time_sleep = random.randint(0, 3)
-                        # 随机延时，避免百度安全机制识别
-                        time.sleep(time_sleep)
-                        # 获取百度链接跳转之后的URL
-                        real_url = baidu_url.headers['location']
-                        b_url_responese = requests.get(url=real_url, headers=headersParameters1, allow_redirects=False, timeout=3)
-                        # 循环直到不是302
-                        if b_url_responese.status_code == 302 or b_url_responese.status_code == 301 or b_url_responese.status_code == 303:
-                            # 对获取到的URL进行访问
-                            #real_url = last_url
-                            driver.set_page_load_timeout(10)  # 设置最大超时时间
-                            driver.set_script_timeout(10)
-                            try:
-                                driver.get(real_url)  # 新建标签
-                                last_url = driver.current_url
-                            except Exception as e:
-                                print("☀✘✘✘----- Domain name not accessible!!!", last_url)
-                            else:
-                                pass
-                            finally:
-                                pass
-                            # time.sleep(2)
-                            if last_url:
-                                print("200---", last_url)
-                            else:
-                                print("☀✘✘✘----- Read timeout!!!")
-                        elif b_url_responese.status_code == 200:
-                            last_url = real_url
-                            print("200---", last_url)
-                        else:
-                            #last_url = b_url_responese.status_code + "is not setting"
-                            print(b_url_responese.status_code, "is not setting")  # 除301,302,200,外未设置的url
-                        f.write(last_url + '\n')
+                        try:
+                            # 如果有推荐百度搜索div块进行+1
+                            get_link_xpath = "/html/body/div[1]/div[3]/div[1]/div[3]/div[" + str(num + 1) + "]/h3/a"
+                            div_del = num + 1
+                            b = driver.find_element_by_xpath(get_link_xpath).get_attribute('href')
+                            print("☀✔✔✔✔✔✔✔✔✔ Read the next div block ", get_link_xpath)  # 此时出现相关搜索的div块，对此div块已加1
+                            results_list.append(b)
+                            #print("error------------------", get_link_xpath)
+                        except Exception as e:
+                            print("-------------------------------There is no relevant link !!!")
+                            break
                     except Exception as e:
-                        txt = str(results_list.index(r)) + " " + r + " " + str(e)
-                        f1.write(txt)
-                        #raise e
-                        print("☀✘✘✘----- Connect error !!!", real_url)
+                        print("☀✘✘✘----- no !!! ", get_link_xpath)
+                    else:
+                        pass
                     finally:
                         pass
+                finally:
+                    pass
+    geturl(results_list)
     driver.quit()
+# 对获取的百度链接进行处理
+# label.begin
+
+
+def geturl(results_list):
+    with open('result.txt', 'w', encoding='utf-8') as f:
+        with open('log.txt', 'w', encoding='utf-8') as f1:
+            print("\n☀☀☀ Get the real url -----------------------------------------------------------------")
+            for r in results_list:
+                try:
+                    last_url = ""
+                    headersParameters1 = random_userAgent(headersParameters)
+                    # 对获取的含有baidu连接的URL进行跳转
+                    baidu_url = requests.get(url=r, headers=headersParameters1, allow_redirects=False, timeout=3)
+                    time_sleep = random.randint(0, 3)
+                    # 随机延时，避免百度安全机制识别
+                    time.sleep(time_sleep)
+                    # 获取百度链接跳转之后的URL
+                    real_url = baidu_url.headers['location']
+                    b_url_responese = requests.get(url=real_url, headers=headersParameters1, allow_redirects=False, timeout=3)
+                    # 循环直到不是302
+                    if b_url_responese.status_code == 302 or b_url_responese.status_code == 301 or b_url_responese.status_code == 303:
+                        # 对获取到的URL进行访问
+                        #real_url = last_url
+                        driver.set_page_load_timeout(10)  # 设置最大超时时间
+                        driver.set_script_timeout(10)
+                        try:
+                            driver.get(real_url)  # 新建标签
+                            last_url = driver.current_url
+                        except Exception as e:
+                            print("☀✘✘✘----- Domain name not accessible!!!", last_url)
+                        else:
+                            pass
+                        finally:
+                            pass
+                        # time.sleep(2)
+                        if last_url:
+                            print("200---", last_url)
+                        else:
+                            print("☀✘✘✘----- Read timeout!!!")
+                    elif b_url_responese.status_code == 200:
+                        last_url = real_url
+                        print("200---", last_url)
+                    else:
+                        #last_url = b_url_responese.status_code + "is not setting"
+                        print(b_url_responese.status_code, "is not setting")  # 除301,302,200,外未设置的url
+                    f.write(last_url + '\n')
+                except Exception as e:
+                    txt = str(results_list.index(r)) + " " + r + " " + str(e)
+                    f1.write(txt)
+                    #raise e
+                    print("☀✘✘✘----- Connect error !!!", real_url)
+                finally:
+                    pass
+    # driver.quit()
 
 
 def main():
+    global start
     help = """
 ☀☀☀ Crawl Baidu Pages [Version is 1.0] ☀☀☀
     
@@ -226,7 +285,7 @@ The scan results are saved in the current directory and the file name is result.
         sys.exit()
     try:
         opts, args = getopt.getopt(sys.argv[1:], '-k:-p:-h-v', ['keyword', 'pages', 'help', 'version'])
-        keyword1 = ''
+        keyword1 = r''
         num = 5
         for opt_name, opt_value in opts:
             if opt_name in ('-k', '--keyword'):
@@ -237,12 +296,13 @@ The scan results are saved in the current directory and the file name is result.
                 print(help)
                 sys.exit()
             if opt_name in ('-v', '--version'):
-                print('\n☀☀☀ Crawl Baidu Pages [Version is 1.0] ☀☀☀')
+                print('\n☀☀☀ Crawl Baidu Pages [Version is 3.0] ☀☀☀')
                 sys.exit()
         if len(keyword1) > 0:
             # print(len(keyword1))
             #print(num, keyword1)
             start = datetime.datetime.now()
+            print("start time ", start)
             print('\n☀☀☀ Crawl Baidu Pages [Version is 1.0] ☀☀☀')
             run(driver_path, keyword=keyword1, num=int(num))
             end = datetime.datetime.now()
